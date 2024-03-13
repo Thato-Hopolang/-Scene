b70ea6fc7eb8d2e8d1ea75171c637211221e9734
Thato Botsane <thbjr@outlook.com>
2024-03-13 21:13:32 +0200

    Modifies App.jsx and App.css

--- autosalon/src/App.jsx
+++ autosalon/src/App.jsx
@@ -4,6 +4,19 @@ function App() {
 
   return (
     <>
+        {/* <div>
+      <Navigation/>
+      <div className="middle">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Servicespage" element={<Servicespage />} />
+          <Route path="/Projectspage" element={<Projectspage />} />
+          <Route path="/Aboutpage" element={<Aboutpage />} />
+          <Route path="/Contactpage" element={<Contactpage />} />
+        </Routes>
+      </div>
+      <Footer/>
+    </div> */}
     </>
   )
 }
