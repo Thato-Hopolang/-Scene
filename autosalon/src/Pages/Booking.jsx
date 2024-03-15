9ddea8681db629e251dd53df651904787afdcc04
Thato Botsane <thbjr@outlook.com>
2024-03-15 22:40:50 +0200

    Adds essential pages.

---
+++ autosalon/src/Pages/Booking.jsx
@@ -0,0 +1,10 @@
+
+const Booking = () => {
+  return (
+    <div>
+      <h1>We are booked</h1>
+    </div>
+  )
+}
+
+export default Booking
---
+++ autosalon/src/Pages/Services.jsx
@@ -0,0 +1,10 @@
+
+const Services = () => {
+  return (
+    <div>
+      <h1>What we have on offer</h1>
+    </div>
+  )
+}
+
+export default Services
