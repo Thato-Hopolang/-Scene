8d83c89e20a81f34e1a6db2b4bb69abc81325776
Thato Botsane <thbjr@outlook.com>
2024-03-16 16:56:26 +0200

    Modifies App.jsx

--- autosalon/src/App.jsx
+++ autosalon/src/App.jsx
@@ -1,22 +1,26 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Navigation from "./Components/Navigation"
+import Footer from './Components/Footer/Footer'
+import Home from "./Pages/Home"
+import Services from "./Pages/Contact"
+import Contact from "./Pages/Contact"
 
 function App() {
 
   return (
     <>
-        {/* <div>
+      <div>
       <Navigation/>
-      <div className="middle">
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/Servicespage" element={<Servicespage />} />
-          <Route path="/Projectspage" element={<Projectspage />} />
-          <Route path="/Aboutpage" element={<Aboutpage />} />
-          <Route path="/Contactpage" element={<Contactpage />} />
-        </Routes>
-      </div>
+        <div className="middle">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Servicespage" element={<Services />} />
+            <Route path="/Contactpage" element={<Contact />} />
+          </Routes>
+        </div>
       <Footer/>
-    </div> */}
+    </div>
     </>
   )
 }
