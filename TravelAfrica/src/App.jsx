e7b9b89e2fd771c311973715fe6c027fe3d59c60
Thato Botsane <thbjr@outlook.com>
2024-03-17 09:37:31 +0200

    Fixes bugs related to App.jsx

--- TravelAfrica/src/App.jsx
+++ TravelAfrica/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Navigation from "./Components/Navigation"
-import Footer from './Components/Footer/Footer'
+import Navigation from "./Components/Navigation/Navigation"
+import Footer from "./Components/Footer/Footer"
 import Home from "./Pages/Home"
 import Services from "./Pages/Contact"
 import Contact from "./Pages/Contact"
